bbb3d72c5c836faaec75e2ecdaaa6a35155dcea2
Gabriel Linares <sgabriellinares@gmail.com>
2026-04-12 22:49:11 -0500

    Hey chavales

---
+++ src/main/java/com/example/tarea_clase3_20220851/TareaClase320220851Application.java
@@ -0,0 +1,13 @@
+package com.example.tarea_clase3_20220851;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TareaClase320220851Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TareaClase320220851Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tarea_clase3_20220851/TareaClase320220851ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tarea_clase3_20220851;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TareaClase320220851ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
